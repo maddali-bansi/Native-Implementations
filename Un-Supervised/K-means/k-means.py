e7622b5f378e2e47b0ad56dc343d5ee7914a706d
maddali-bansi <maddali.bansi@gmail.com>
2022-03-22 16:40:46 +0530

    Completed native K-Means in 2D

--- Un-Supervised/K-means/k-means.py
+++ Un-Supervised/K-means/k-means.py
@@ -1,5 +1,5 @@
 # Author Bansi Maddali
-
+import numpy.random
 import pandas as pd
 import math
 
@@ -13,9 +13,11 @@ def k_means(k_clusters, data_df):
     clusters = data_df.sample(k_clusters, replace=False)
     data_df['cluster'] = 0
     converge = False
+    iteration = 0
     assigned_cluster = 0
     while not converge:
         # Assign Clusters
+        # print("Clusters = ", clusters)
         for index, datapoint in data_df.iterrows():
             minimum = math.inf
 
@@ -33,19 +35,30 @@ def k_means(k_clusters, data_df):
                     minimum = distance
                     data_df.at[index, 'cluster'] = cluster_id
                 cluster_id += 1
-        print(data_df)
-        print(clusters)
-        print("=============================")
-        if clusters.equals(data_df.groupby('cluster').mean()):
+
+        # print(data_df)
+        # print(clusters)
+        # print("=============================")
+        if iteration != 0 and prev_df.equals(data_df):
             converge = True
         else:
+            # Re-Compute Centroids(Clusters)
             clusters = data_df.groupby('cluster').mean()
+            prev_df = data_df
+        iteration += 1
+    return data_df, clusters, iteration
 
 
 if __name__ == '__main__':
-    k = 3
-    data = {'x': [800, 10, 30, 40, 500],
-            'y': [900, 20, 40, 50, 600]
+    k = 50
+    data = {'x': numpy.random.randint(low=1, high=10, size=1000),
+            'y': numpy.random.randint(low=2, high=8, size=1000)
             }
     df = pd.DataFrame(data)
-    k_means(k, df)
+    df, cluster_centers, itr = k_means(k, df)
+    print("K Mean converged after ", itr, " iterations : ")
+    # print("============")
+    # print("Clustered Rows")
+    # print(df)
+    # print("Cluster Co-ordinates")
+    print(cluster_centers)
